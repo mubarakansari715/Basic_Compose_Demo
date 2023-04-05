2b9ac3c2827c21fd016bab16b5e3ef769e1eee0d
mubarak_ansari <mubarak.ansari@brainvire.com>
2023-04-05 13:13:40 +0530

    Calling api using flow

--- app/src/main/java/com/mubarak/basic_compose_demo/post/ui/PostDesignScreen.kt
+++ app/src/main/java/com/mubarak/basic_compose_demo/post/ui/PostDesignScreen.kt
@@ -3,7 +3,6 @@ package com.mubarak.basic_compose_demo.post.ui
 import android.content.Context
 import android.util.Log
 import android.widget.Toast
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,89 +10,67 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Scaffold
 import androidx.compose.runtime.*
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import com.mubarak.basic_compose_demo.MainActivity
+import androidx.lifecycle.viewModelScope
 import com.mubarak.basic_compose_demo.home.ItemsClickedInterface
-import com.mubarak.basic_compose_demo.navigation.MobileNavigation
 import com.mubarak.basic_compose_demo.network.ResponseHandler
 import com.mubarak.basic_compose_demo.post.item.PostItemDesign
 import com.mubarak.basic_compose_demo.post.model.Post
 import com.mubarak.basic_compose_demo.post.viewmodel.PostViewModel
-import com.mubarak.basic_compose_demo.utils.TopBarManage
+import kotlinx.coroutines.launch
 
 @Composable
 fun PostDesignScreen(
     appContext: Context
 ) {
-
+    val context = LocalContext.current
     val viewModel = remember { PostViewModel() }
-    val postObserverState by viewModel.posts.observeAsState()
+    //val postObserverState by viewModel.posts.observeAsState()
     var isLoading by remember { mutableStateOf(false) }
+    val postList = viewModel.postsList
 
     /***
      * Show Loader on Screen
      */
     LoaderShow(isLoading)
 
-    when (postObserverState) {
+    LaunchedEffect(Unit) {
+        viewModel.viewModelScope.launch {
+            viewModel.posts.collect { postObserverState ->
 
-        is ResponseHandler.Loading -> {
-            LaunchedEffect(Unit) {
-                isLoading = true
-                // perform the loading operation here
-                //isLoading = false
-            }
-        }
-        is ResponseHandler.Empty -> {
+                when (postObserverState) {
 
-        }
-        is ResponseHandler.OnFailed -> {
+                    is ResponseHandler.Loading -> {
+                        isLoading = true
+                    }
+                    is ResponseHandler.Empty -> {
 
-        }
-        is ResponseHandler.OnSuccessResponse<List<Post>> -> {
-            isLoading = false
-            (postObserverState as ResponseHandler.OnSuccessResponse<List<Post>>).response.let {
+                    }
+                    is ResponseHandler.OnFailed -> {
 
-                Log.e("@@@mubarak", "PostDesignScreen: $it")
+                    }
+                    is ResponseHandler.OnSuccessResponse<List<Post>> -> {
+                        isLoading = false
+                        postObserverState.response.let {
+                            viewModel.setPostList(it)
+                            Log.e("@@@mubarak", "PostDesignScreen: $it")
 
-                LazyColumn(
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .fillMaxWidth()
-                ) {
-                    /*items(
-                        items = it,
-                        itemContent = {
-                            PostItemDesign(it)
+
                         }
-                    )*/
+                    }
 
-                    itemsIndexed(
-                        items = it,
-                        itemContent = { position, data ->
-                            PostItemDesign(position, data, object : ItemsClickedInterface {
-                                override fun clickViewItems() {
-                                    Toast.makeText(appContext, data.body, Toast.LENGTH_SHORT)
-                                        .show()
-                                }
-
-                                override fun clickAddToCartButton() {
-                                    Toast.makeText(appContext, data.title, Toast.LENGTH_SHORT)
-                                        .show()
-                                }
-                            })
-                        }
-                    )
                 }
+
             }
         }
-
     }
+
+    DataPost(context = context, post = postList)
+
 }
 
 @Composable
@@ -113,5 +90,32 @@ fun LoaderShow(isLoading: Boolean) {
             topBar = { TopBarManage("Post Listing") },
             content = {  })*/
 
+    }
+}
+
+@Composable
+fun DataPost(context: Context, post: List<Post>) {
+    LazyColumn(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+    ) {
+
+        itemsIndexed(
+            items = post,
+            itemContent = { position, data ->
+                PostItemDesign(position, data, object : ItemsClickedInterface {
+                    override fun clickViewItems() {
+                        Toast.makeText(context, data.body, Toast.LENGTH_SHORT)
+                            .show()
+                    }
+
+                    override fun clickAddToCartButton() {
+                        Toast.makeText(context, data.title, Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                })
+            }
+        )
     }
 }
--- app/src/main/java/com/mubarak/basic_compose_demo/post/viewmodel/PostViewModel.kt
+++ app/src/main/java/com/mubarak/basic_compose_demo/post/viewmodel/PostViewModel.kt
@@ -1,21 +1,32 @@
 package com.mubarak.basic_compose_demo.post.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mubarak.basic_compose_demo.network.ResponseHandler
 import com.mubarak.basic_compose_demo.post.model.Post
 import com.mubarak.basic_compose_demo.post.repository.PostRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class PostViewModel : ViewModel() {
 
     private val postRepository = PostRepository()
 
-    private val _posts = MutableLiveData<ResponseHandler<List<Post>>>()
-    val posts: LiveData<ResponseHandler<List<Post>>>
-        get() = _posts
+    /* private val _posts = MutableLiveData<ResponseHandler<List<Post>>>()
+     val posts: LiveData<ResponseHandler<List<Post>>>
+         get() = _posts*/
+
+    private val _posts = MutableStateFlow<ResponseHandler<List<Post>>>(ResponseHandler.Empty)
+    val posts = _posts as StateFlow<ResponseHandler<List<Post>>>
+
+    private val _postsList :MutableList<Post> = mutableStateListOf()
+    val postsList :List<Post> = _postsList
+
+    fun setPostList(list:List<Post>) {
+        _postsList.addAll(list)
+    }
 
     init {
         getPosts()
